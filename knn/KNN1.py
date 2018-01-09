c79ef0258836ef5c6d2ae0c290bfa3c457c04121
lebronjames <798258843@qq.com>
2018-01-09 14:45:33 +0800

    20180109

---
+++ knn/KNN1.py
@@ -0,0 +1,23 @@
+from sklearn import neighbors
+#from sklearn import datasets
+from sklearn.datasets import load_iris
+
+knn = neighbors.KNeighborsClassifier() #获取knn分类算法
+
+#iris = datasets.load_iris() #把数据load进来
+
+#查看iris数据集
+iris = load_iris()
+
+print(iris)
+
+#训练数据集
+#knn.fit(iris.data,iris.target)
+
+#预测
+#predict = knn.predict([[0.1,0.2,0.3,0.4]])
+
+#print(predict)
+
+
+
