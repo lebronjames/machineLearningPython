b7787bda837ab59663893de33fb5797bda12926d
lebronjames <798258843@qq.com>
2018-01-09 16:13:35 +0800

    20180109

--- knn/KNN2.py
+++ knn/KNN2.py
@@ -5,39 +5,40 @@ import operator  #
 from sklearn import neighbors
 
 #计算距离
-def euclideanDistance(instance1,instance2):
+def euclideanDistance(instance1,instance2,length):
     distance = 0
-    for x in range(4):
+    for x in range(length):
         distance += pow((instance1[x]-instance2[x]),2)
     return math.sqrt(distance)
 
 #返回K个最近邻
 def getNeighbors(trainingSet,testInstance,k):
     distances = []
-    # 计算每一个测试实例到训练集实例的距离
+    length = len(testInstance) -1
+    #计算每一个测试实例到训练集实例的距离
     for x in range(len(trainingSet)):
-        dist = euclideanDistance(testInstance, trainingSet[x])
-        distances.append((trainingSet[x], dist))
-    # 对所有的距离进行排序
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x],dist))
+    #对所有的距离进行排序
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
-    # 返回k个最近邻
+    #返回k个最近邻
     for x in range(k):
         neighbors.append(distances[x][0])
     return neighbors
 
 #对k个近邻进行合并，返回value最大的key
-def getResult(neighbors):
-    classVotes = {}
+def getResponse(neighbors):
+    votes = {}
     for x in range(len(neighbors)):
         response = neighbors[x][-1]
-        if response in classVotes:
-            classVotes[response] += 1
+        if response in votes:
+            votes[response] += 1
         else:
-            classVotes[response] = 1
-    # 排序
-    sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedVotes[0][0]
+            votes[response] = 1
+    #排序
+    sortedVotes = sorted(votes,key = operator.itemgetter(1),reverse =True)
+    return sortedVotes[0]
 
 #计算准确率
 def getAccuracy(testSet,predictions):
@@ -49,7 +50,7 @@ def getAccuracy(testSet,predictions):
 
 #加载数据集
 def loadDataset(filename,split,trainingSet=[],testSet = []):
-    with open(filename,"rb") as csvfile:
+    with open(filename,"rt") as csvfile:
         lines = csv.reader(csvfile)
         dataset = list(lines)
         for x in range(len(dataset)-1):
@@ -65,22 +66,18 @@ def main():
     testSet = []  # 测试数据集
     split = 0.67  # 分割的比例
     loadDataset(r"./iris.txt", split, trainingSet, testSet)
-    print
-    "Train set :" + repr(len(trainingSet))
-    print
-    "Test set :" + repr(len(testSet))
+    print("Train set :" + repr(len(trainingSet)))
+    print("Test set :" + repr(len(testSet)))
 
     predictions = []
     k = 3
     for x in range(len(testSet)):
         neighbors = getNeighbors(trainingSet, testSet[x], k)
-        result = getResult(neighbors)
+        result = getResponse(neighbors)
         predictions.append(result)
-        print
-        ">predicted = " + repr(result) + ",actual = " + repr(testSet[x][-1])
+        print(">predicted = " + repr(result) + ",actual = " + repr(testSet[x][-1]))
     accuracy = getAccuracy(testSet, predictions)
-    print
-    "Accuracy:" + repr(accuracy) + "%"
+    print("Accuracy:" + repr(accuracy) + "%")
 
 if __name__ =="__main__":
     main()
